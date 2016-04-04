f0d438ba342e034c5776b43d7f4879a24d760ad1
ejaz <ejaz_ahmed@outlook.com>
2016-04-04 15:21:55 +0500

    Updating gitignore and cleaning up example Controller

--- rest-awesome-example/grails-app/controllers/demo/BookController.groovy
+++ rest-awesome-example/grails-app/controllers/demo/BookController.groovy
@@ -9,13 +9,4 @@ class BookController extends AwesomeRestfulController {
     BookController(){
         super(Book)
     }
-
-//    @Override
-//    def index(Integer max) {
-//        params.max = Math.min(max ?: 10, 100)
-//        def detail = params.detail ?: "complete"
-//        respond Book.list(params), [detail:detail, paging:[totalCount: Book.count(),
-//                                                            currentMax: params.max,
-//                                                            curentOffset: params.offset ?: 0]]
-//    }
 }
